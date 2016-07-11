e9f5e01149f3182fb41da33e4745acbbad37ea2a
Courtney Noonan <courtneynoonan@me.com>
2016-07-11 14:15:40 -0400

    adding one kitten to the yard

--- cats.js
+++ cats.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
+
   $('button').click(function(event){
     $.ajax({
       url: 'http://bitkittens.herokuapp.com/cats.json',
       method: 'GET',
       data: { },
       dataType: 'json'
+    }).done(function(responseData){
+      var kittenJSON = responseData;
+      var kittenList = kittenJSON["cats"];
+      var catBox1 = document.createElement('img');
+      cat = kittenList[0];
+      console.log(cat.photo);
+      $('#cat1').append(catBox1);
+      $('#cat1 img').attr('src', cat.photo);
+
+
     });
   });
+
 });
